836428b1de109ee22e31237518fca3988a33ef26
thiagocaandrade <thiago.carlos.andrade@gmail.com>
2020-07-09 17:32:27 -0300

    Initial commit

---
+++ app/src/main/java/com/example/convidados/ui/gallery/PresentFragment.kt
@@ -0,0 +1,31 @@
+package com.example.convidados.ui.gallery
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.example.convidados.R
+
+class PresentFragment : Fragment() {
+
+    private lateinit var presentViewModel: PresentViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        presentViewModel =
+                ViewModelProviders.of(this).get(PresentViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_gallery, container, false)
+        val textView: TextView = root.findViewById(R.id.text_gallery)
+        presentViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
---
+++ app/src/main/java/com/example/convidados/ui/slideshow/AbsentFragment.kt
@@ -0,0 +1,31 @@
+package com.example.convidados.ui.slideshow
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.example.convidados.R
+
+class AbsentFragment : Fragment() {
+
+    private lateinit var absentViewModel: AbsentViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        absentViewModel =
+                ViewModelProviders.of(this).get(AbsentViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_slideshow, container, false)
+        val textView: TextView = root.findViewById(R.id.text_slideshow)
+        absentViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
