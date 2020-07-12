5333150506417e86716acde60d06664328c4edb8
thiagocaandrade <thiago.carlos.andrade@gmail.com>
2020-07-12 16:30:58 -0300

    create SQLite, Singleton, Insert

--- app/src/main/java/com/example/convidados/view/AbsentFragment.kt
+++ app/src/main/java/com/example/convidados/view/AbsentFragment.kt
@@ -16,13 +16,13 @@ class AbsentFragment : Fragment() {
     private lateinit var absentViewModel: AbsentViewModel
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         absentViewModel =
-                ViewModelProviders.of(this).get(AbsentViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_slideshow, container, false)
+            ViewModelProviders.of(this).get(AbsentViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_absent, container, false)
         val textView: TextView = root.findViewById(R.id.text_slideshow)
         absentViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
--- app/src/main/java/com/example/convidados/view/AllGuestsFragment.kt
+++ app/src/main/java/com/example/convidados/view/AllGuestsFragment.kt
@@ -16,13 +16,13 @@ class AllGuestsFragment : Fragment() {
     private lateinit var homeViewModel: AllGuestsViewModel
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         homeViewModel =
-                ViewModelProviders.of(this).get(AllGuestsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
+            ViewModelProviders.of(this).get(AllGuestsViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_all, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
