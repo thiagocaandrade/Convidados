f9ac08ba6ff11e5e6f2b0a48511015342ad7545c
thiagocaandrade <thiago.carlos.andrade@gmail.com>
2020-07-13 13:14:20 -0300

    add Room

---
+++ app/src/main/java/com/example/convidados/service/repository/GuestDatabase.kt
@@ -0,0 +1,29 @@
+package com.example.convidados.service.repository
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.convidados.service.model.GuestModel
+
+@Database(entities = [GuestModel::class], version = 1)
+abstract class GuestDatabase : RoomDatabase(){
+
+    abstract fun guestDAO(): GuestDAO
+
+    companion object {
+
+        private lateinit var INSTANCE: GuestDatabase
+        fun getDatabase(context: Context): GuestDatabase{
+            if (!::INSTANCE.isInitialized){
+                synchronized(GuestDatabase::class.java) {
+                    INSTANCE = Room.databaseBuilder(context, GuestDatabase::class.java, "guestDB")
+                        .allowMainThreadQueries()
+                        .build()
+                }
+            }
+            return INSTANCE
+        }
+    }
+
+}
